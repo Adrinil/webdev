b9101d534cd42127e8516c096dbc6dbe7b430471
Adrinil Sahoo <131552713+Adrinil@users.noreply.github.com>
2025-09-29 04:05:15 +0530

    Add files via upload

---
+++ project 9/display.js
@@ -0,0 +1,14 @@
+```javascript
+// Display.js
+import React from "react";
+
+function Display({ count }) {
+  return (
+    <div>
+      <h2>Current Count: {count}</h2>
+    </div>
+  );
+}
+
+export default Display;
+```
